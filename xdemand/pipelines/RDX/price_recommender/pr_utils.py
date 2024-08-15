3ab06940510c7f0d42fc858d3224834fe34f6edc
Ahmed Jawad <jawwada@gmail.com>
2024-08-15 09:39:58 +0200

    price adjustment function

--- xdemand/pipelines/RDX/price_recommender/pr_utils.py
+++ xdemand/pipelines/RDX/price_recommender/pr_utils.py
@@ -81,3 +81,33 @@ def calculate_adjusted_price_stock( df):
             running_stock -= predicted_sales
 
     return df
+
+def get_price_adjustments(name,group, p0, price_rec, r, cf):
+
+    group_info = dict({
+        'sku': name[0],
+        'warehouse_code': name[1],
+        'ref_price': group['ref_price'].mean(),
+        'mean_demand': group['yhat'].mean(),
+        'current_stock': group['running_stock_after_forecast'].head(1).item(),
+        'understock_days': group['is_understock'].sum(),
+        'overstock_days': group['is_overstock'].sum(),
+        'price_elasticity': group['price_elasticity'].mean()
+    })
+    group['q_prime'] = group['yhat'] * (price_rec / p0) ** r
+    group = calculate_adjusted_price_stock(group)
+    group['y_hat_adj'] = group['yhat'].where(group['yhat'] < group['running_stock_after_forecast'], 0)
+    group['q_prime_adj'] = group['q_prime'].where(group['q_prime'] < group['running_stock_after_forecast_adj'], 0)
+    revenue_before = group['y_hat_adj'].sum() * p0
+    revenue_after = group['q_prime_adj'].sum() * price_rec
+    # set up the df_sku_warehouse_info
+
+    # Add new columns to the aggregated DataFrame
+    group_info['revenue_before'] = revenue_before
+    group_info['revenue_after'] = revenue_after
+    group_info['price_new'] = price_rec
+    group_info['price_old'] = p0
+    group_info['opt_stock_level'] = group_info['mean_demand'] * cf.forecast_stock_level
+    df_group_info = pd.DataFrame(group_info, index=[0])
+    print(revenue_before, revenue_after)
+    return group, df_group_info
