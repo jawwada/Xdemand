9b56842beaa5b61867af75f4a1c8f7f164083cff
Ahmed Jawad <jawwada@gmail.com>
2024-08-15 10:37:13 +0200

    output parsing errors handled with custom call backs. in this branch we also have price adjustments code handled with some refactorings

--- xiom_optimized/chat_agent.py
+++ xiom_optimized/chat_agent.py
@@ -92,5 +92,6 @@ agent_running_stock = create_pandas_dataframe_agent(
     verbose=False,
     agent_type=AgentType.OPENAI_FUNCTIONS,
     number_of_head_rows=5,
-    allow_dangerous_code=True
+    handle_output_parsing_errors=True,
+    allow_dangerous_code=True,
 )
