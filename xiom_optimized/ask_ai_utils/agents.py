353cc31f2ace44982b6032e6d01edd504348e8fb
Ahmed Jawad <jawwada@gmail.com>
2024-08-15 19:29:37 +0200

    very good refactored but slightly not working

--- xiom_optimized/ask_ai_utils/agents.py
+++ xiom_optimized/ask_ai_utils/agents.py
@@ -30,6 +30,7 @@ agent_running_stock = create_pandas_dataframe_agent(
 agent_visualisation = LLMChain(
     llm=ChatOpenAI(temperature=0.2, model="gpt-4o-mini"),
     prompt=prompt_template_visualisation_engineer)
+
 agent_data_table = LLMChain(
     llm=ChatOpenAI(temperature=0.2, model="gpt-4o-mini"),
     prompt=prompt_template_final_df)
