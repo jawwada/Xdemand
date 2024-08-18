37c27e483047a2260f8c02c052c95aab8c10dd68
Ahmed Jawad <jawwada@gmail.com>
2024-08-18 17:00:48 +0200

    remove extra joblib directory

--- common/local_constants.py
+++ common/local_constants.py
@@ -1,7 +1,4 @@
-from joblib import Memory
-local_cache_directory = "cache/cache_directory"
-memory = Memory(local_cache_directory, verbose=0)
-memory.timeout = 60*60*24*7
+
 
 region_warehouse_codes = {
     'USA': 'US',
