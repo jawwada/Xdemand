134c940a2372702abebf5a2a31a647cfcf8ba136
Ahmed Jawad <jawwada@gmail.com>
2024-08-22 06:13:45 +0200

    adding template

--- xiom_optimized/langchain_utils/agents.py
+++ xiom_optimized/langchain_utils/agents.py
@@ -3,7 +3,7 @@ from langchain.chains import LLMChain
 from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
 from langchain_openai import ChatOpenAI
 
-from xiom_optimized.langchain_utils.prompts import prompt_template_final_df
+from xiom_optimized.langchain_utils.prompts import prompt_template_final_df, prompt_template_explain_page
 from xiom_optimized.langchain_utils.prompts import prompt_template_visualisation_engineer
 from xiom_optimized.utils.data_fetcher import df_agg_monthly_3years
 from xiom_optimized.utils.data_fetcher import df_price_rec_summary
