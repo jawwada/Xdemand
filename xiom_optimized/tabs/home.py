e77b784a796c1d67579cb3afcf5901104a453e48
Ahmed Jawad <jawwada@gmail.com>
2024-08-25 16:08:49 +0200

    creating prompt_news.py

--- xiom_optimized/tabs/home.py
+++ xiom_optimized/tabs/home.py
@@ -85,7 +85,6 @@ from xiom_optimized.langchain_utils.prompts import prompt_ds
 def update_news_output(pathname):
     if pathname == "/":
         return html.Div([
-            html.P("Loading news...", id="loading-news"),
             dcc.Loading(
                 id="loading-news-spinner",
                 children=[html.Div(id="news-content")],
